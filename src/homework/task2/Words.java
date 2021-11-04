f3e158b1fe45b468ce437a34112d7b25db87801e
vevdokimenko <evdokimenko@hotmail.com>
2021-11-04 17:08:34 +0200

    Homework task2

---
+++ src/homework/task2/Words.java
@@ -0,0 +1,37 @@
+package homework.task2;
+
+/*
+* Задание 2
+Используя коллекцию удвойте слово:
+1.	Введите с клавиатуры 5 слов в список строк.
+2.	Метод doubleValues должен удваивать слова по принципу a,b,c -> a,a,b,b,c,c.
+3.	Используя цикл for выведите результат на экран, каждое значение с новой строки.
+* */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Words {
+    public List<String> doubleValues(List<String> words) {
+        List<String> newList = new ArrayList<>();
+        for (String word : words) {
+            newList.add(word);
+            newList.add(word);
+        }
+        return newList;
+    }
+
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            words.add(new Scanner(System.in).nextLine());
+        }
+
+        List<String> doubleWords = new Words().doubleValues(words);
+
+        for (String item : doubleWords) {
+            System.out.println(item);
+        }
+    }
+}
